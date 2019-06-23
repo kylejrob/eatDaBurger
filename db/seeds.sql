9cefdae1bac0b054cf2cca3158409af6829089b0
Kyle <kylejrob@gmail.com>
2019-06-23 12:45:12 -0600

    initialing

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Red Robin Royal Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western BBQ Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Denver Burger', true);
